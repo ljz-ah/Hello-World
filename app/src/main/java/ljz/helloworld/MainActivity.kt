7d9b51c41a60ee721eab820568b8f9171d515a11
liujz <1224947053@qq.com>
2020-01-15 14:35:20 +0800

    test03

--- app/src/main/java/ljz/helloworld/MainActivity.kt
+++ app/src/main/java/ljz/helloworld/MainActivity.kt
@@ -2,6 +2,7 @@ package ljz.helloworld
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    
+
     }
 
-    fun test(){
+    fun test(v:View){
 
         tv_content.text="I  LOVE  YOU "
 
